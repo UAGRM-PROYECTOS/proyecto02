54c49c011ebb256f31e1571d6481a491cf155728
Ali1715 <tacoomaturanoalison@gmail.com>
2024-08-10 21:27:28 -0400

    comando table

--- app/Models/Comando.php
+++ app/Models/Comando.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Comando extends Model
-{
-    protected $fillable = ['caso_de_uso', 'accion', 'parametro', 'ejemplo'];
-}
+    class Comando extends Model
+    {
+        protected $table = 'comando';
+        protected $fillable = ['caso_de_uso', 'accion', 'parametro', 'ejemplo'];
+    }
